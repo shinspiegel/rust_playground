52907c34115fd022df7600a411cdd4c075d6bdbf
Jeferson 'Shin <shin.jef@gmail.com>
2021-02-14 10:25:49 -0300

    Added take two makes ten

---
+++ two_make_ten/target/debug/deps/two_make_ten-590e561ca076c095.d
@@ -0,0 +1,6 @@
+/Users/shin/Documents/Personal/Code/RUST/rust_playground/two_make_ten/target/debug/deps/two_make_ten-590e561ca076c095: src/main.rs src/two_make_ten.rs
+
+/Users/shin/Documents/Personal/Code/RUST/rust_playground/two_make_ten/target/debug/deps/two_make_ten-590e561ca076c095.d: src/main.rs src/two_make_ten.rs
+
+src/main.rs:
+src/two_make_ten.rs:
